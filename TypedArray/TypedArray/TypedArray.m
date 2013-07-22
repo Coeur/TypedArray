c8249e04984349d7ed5d41765090bf38ad1848ed
antoine <antoine.vigier@itelios.com>
2013-07-22 03:50:17 +0200

    all init methods are wrappers for initWithObjects:count:

--- TypedArray/TypedArray/TypedArray.m
+++ TypedArray/TypedArray/TypedArray.m
@@ -40,96 +40,6 @@
         self = nil;
     return self;
 }
-/*
-// implementation not required as it's just a wrapper for initWithObjects:count:
-- (instancetype)initWithObjects:(id)firstObj, ... NS_REQUIRES_NIL_TERMINATION
-{
-    self = [super init];
-    if (!self)
-        return nil;
-    
-    va_list args;
-    
-    va_start(args, firstObj);
-    NSUInteger count = 0;
-    for (id obj = firstObj; obj != nil; obj = va_arg(args, id)) {
-        count++;
-    }
-    va_end(args);
-    
-    if (count == 0)
-    {
-        self.array = [[NSArray alloc] initWithObjects:nil count:count];
-    }
-    else
-    {
-        id cArray[count];
-        
-        va_start(args, firstObj);
-        NSUInteger i = 0;
-        for (id obj = firstObj; obj != nil; obj = va_arg(args, id)) {
-            cArray[i++] = obj;
-        }
-        va_end(args);
-        
-        self.array = [[NSArray alloc] initWithObjects:cArray count:count];
-    }
-    
-    if (!_array)
-        self = nil;
-    return self;
-}
-*/
-- (instancetype)initWithArray:(NSArray *)anArray
-{
-    self = [super init];
-    if (!self)
-        return nil;
-    self.array = [[NSArray alloc] initWithArray:anArray];
-    if (!_array)
-        self = nil;
-    return self;
-}
-- (instancetype)initWithArray:(NSArray *)anArray copyItems:(BOOL)flag
-{
-    self = [super init];
-    if (!self)
-        return nil;
-    self.array = [[NSArray alloc] initWithArray:anArray copyItems:flag];
-    if (!_array)
-        self = nil;
-    return self;
-}
-- (instancetype)initWithContentsOfFile:(NSString *)aPath
-{
-    self = [super init];
-    if (!self)
-        return nil;
-    self.array = [[NSArray alloc] initWithContentsOfFile:aPath];
-    if (!_array)
-        self = nil;
-    return self;
-}
-- (instancetype)initWithContentsOfURL:(NSURL *)aURL
-{
-    self = [super init];
-    if (!self)
-        return nil;
-    self.array = [[NSArray alloc] initWithContentsOfURL:aURL];
-    if (!_array)
-        self = nil;
-    return self;
-}
-- (instancetype)initWithCoder:(NSCoder *)decoder;
-{
-    self = [super init];
-    if (!self)
-        return nil;
-    self.array = [[NSArray alloc] initWithCoder:decoder];
-    if (!_array)
-        self = nil;
-    return self;
-}
 
 #pragma mark Methods only defined for abstract class
 
@@ -137,7 +47,6 @@
 {
     return [_array count];
 }
-
 - (id)objectAtIndex:(NSUInteger)index
 {
     return [_array objectAtIndex:index];
